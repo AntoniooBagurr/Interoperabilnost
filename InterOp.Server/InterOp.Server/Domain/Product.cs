2b28e1a2bd61175a821331b79b2663dfaab53c6e
AntoniooBagurr <abagur@algebra.hr>
2025-09-06 11:46:39 +0200

    Prvi commit

---
+++ InterOp.Server/InterOp.Server/Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace InterOp.Server.Domain;
+
+public class Product
+{
+    public int Id { get; set; }            
+    public string ExtId { get; set; } = ""; 
+    public string Title { get; set; } = "";
+    public string? Currency { get; set; }
+    public decimal? Price { get; set; }
+    public string? ShopName { get; set; }
+    public string? Url { get; set; }
+    public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+}
